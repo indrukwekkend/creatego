a54f5266627989442e91d5b588d152f0f6946b56
odilio <github@odil.io>
2017-12-19 14:10:52 +0100

    Overbodige templates verwijderd

--- template-jumbotron.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template Name: Pagina met Jumbotron
- */
-?>
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
